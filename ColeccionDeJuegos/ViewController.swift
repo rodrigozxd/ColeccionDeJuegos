ea6ae5ea04502abf2597f9b2d8e24291d532c71f
rodrigozxd <rodrigo.paredes@tecsup.edu.pe>
2021-11-08 19:13:57 -0800

    Initial Commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by RodrigoParedes on 8/11/21.
+//  Copyright © 2021 RodrigoParedes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
